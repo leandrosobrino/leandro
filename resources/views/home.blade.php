a636448f40bf0e8e4468ec352eb2dbceabbb3f5b
leandrosobrino <sobrinoleandro01@gmail.com>
2019-12-29 20:47:57 -0300

    cambios en el home faltan efectos y organizar mejor algunas cosas pero casi listo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,16 @@
         <div class=" contenedorDeNissan "  ><img class=" fotoNissan" src="/img/nissan3.jpeg"    alt="foto"> </div>
         <div class=" contenedorDeNissan "  ><img class=" fotoNissan" src="/img/nissan2.jpeg" alt="foto"> </div>
      </div>
-     
+
+     <div class="contenedorDe6fotos">
+          <!-- <div class=" contenedorDeMuchasFotos" > <img class=" imgFotoMuchas" src="/img/porsche/porsche2.jpeg " alt="foto" > </div>
+          <div class=" contenedorDeMuchasFotos" > <img class=" imgFotoMuchas" src=" /img/porsche/porsche1.jpeg " alt="foto" > </div>
+          <div class=" contenedorDeMuchasFotos" > <img class=" imgFotoMuchas" src=" /img/porsche/porsche3.jpeg " alt="foto" > </div> -->
+          <div class=" contenedorDeMuchasFotos" > <img class=" imgFotoMuchas" src=" /img/porsche/porsche5.jpeg" alt="foto" > </div>
+          <div class=" contenedorDeMuchasFotos" > <img class=" imgFotoMuchas" src=" /img/porsche/porsche6.jpeg" alt="foto" > </div>
+          <div class=" contenedorDeMuchasFotos" > <img class=" imgFotoMuchas" src=" /img/porsche/porsche4.jpeg" alt="foto" > </div>
+
+     </div>
      
   
     </div>
